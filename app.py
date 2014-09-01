eb192beee0ab79d18e062abc7977ed4fc529ce5d
Brendan Wright <bwright@fb.com>
2014-09-01 15:52:33 -0700

    Form submission is working. Now to do something with the result.

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
   return render_template('index.html') 
+
+@app.route('/submit', methods=['POST'])
+def submit():
+  street = request.form['street']
+  unit = request.form['unit']
+  city = request.form['city']
+  email = request.form['email']
+
+  return ('Received:<br />' +
+    street + '<br />' +
+    unit + '<br />' +
+    city + '<br />' +
+    email )
 
 if __name__ == '__main__':
   # TODO: remove before pushing to server 
